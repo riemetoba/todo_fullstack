28fc714c3bcc12441d23c64c5fafaaba88b32065
Toba <riemehasan06@gmail.com>
2026-06-23 22:26:53 +0600

    create: todoModel

--- backend/models/todoModel.js
+++ backend/models/todoModel.js
@@ -1,1 +1,19 @@
 const mongoose = require ("mongoose")
+const {Schema} = mongoose
+
+const todoSchema = new Schema({
+    task:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        enum: ["pending", "active", "block"],
+        default: "pending"
+    },
+    priority:{
+        type: String,
+        enum: ["high", "medium", "low"],
+        required: true
+    }
+})
