eb3e465e7c582c750a9f2ca4181798d88791f010
Toba <riemehasan06@gmail.com>
2026-06-24 22:18:32 +0600

    update: todo save in database

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -3,4 +3,16 @@ const Todo = require ("../models/todoModel")
 
 const createTodo = (req, res)=>{
     const {task, status, priority} = req.body
+
+    const todo = new Todo({
+    task: task,
+    priority: priority
+})
+
+todo.save()
+
+
+
+
 }
+
