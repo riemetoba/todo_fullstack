f7a56fd67bd7b9ff65c7d353b7979f7643acc232
Toba <riemehasan06@gmail.com>
2026-07-22 22:27:40 +0600

    refactor: move todo routes to separate routes/todoRoutes.js file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
-const {
-  createTodo,
-  allTodos,
-  todoDelete,
-  todoUpdate,
-} = require("./controllers/todoController");
 const cors = require("cors");
 const dbConnection = require("./config/databaseConfig");
-const upload = require ("./utils/storage")
+const todoRoute = require("./routes/todoRoute");
 
 // ============ middleware ==============
 
@@ -22,10 +16,7 @@ dbConnection();
 
 // ====================
 
-app.post("/create/todo", upload.single("image"), createTodo);
-app.get("/allTodos", allTodos);
-app.delete("/delete/:id", todoDelete);
-app.post("/update/:id", todoUpdate);
+app.use("/", todoRoutes);
 
 app.listen(5000, () => {
   console.log("server is running");
